90cc06e50ab794b4904527a93bef2516a8f1b60d
Aditya Kurkure <56596662+epicadk@users.noreply.github.com>
2021-12-12 12:35:11 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  return numbers.reduce((p, c)=> p + c) / numbers.length;
 }
 
 module.exports = {average};
